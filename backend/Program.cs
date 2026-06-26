36d62d4068e345726fbb48b0e598e9dcc8615efc
Diar <diarselmani10@gmail.com>
2026-06-26 12:11:49 +0200

    db

--- backend/Program.cs
+++ backend/Program.cs
@@ -71,6 +71,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 
 app.UseHttpsRedirection();
