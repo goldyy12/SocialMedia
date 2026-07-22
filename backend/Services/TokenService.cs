c0e5efabbacb9e528def15ec114b0b7115d0a819
Diar <diarselmani10@gmail.com>
2026-07-22 13:13:44 +0200

    Refresh

--- backend/Services/TokenService.cs
+++ backend/Services/TokenService.cs
@@ -21,7 +21,7 @@ public class TokenService
             new Claim(JwtRegisteredClaimNames.Email, user.Email),
         };
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Secret"]!));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
         var token = new JwtSecurityToken(
